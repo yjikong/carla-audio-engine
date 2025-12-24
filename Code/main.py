b02c3da9be1802d5775a6b5e3c3e6af3a79971ed
BrezenFan <ozan.mgl@gmail.com>
2025-12-24 19:48:02 +0100

    main found out how to import the modules

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,5 @@
-from FMOD import Data
-from ..Banks import TriggerBank
+from FMOD.Classes.Banks.TriggerBank import * 
+from FMOD.Data_class import *
 
 if __name__ == '__main__':
     #Init
@@ -7,3 +7,13 @@ if __name__ == '__main__':
     TriggerBank.load()
     TriggerBank.prepare_event()
     Data.Data()
+
+    #prevent constant restart of warning sound:
+    Trigger = False
+
+    while True:
+        if Data.get_speed() > Data.get_speed_limit & Trigger == False:
+            TriggerBank.event_inst.start()
+            Trigger = True
+        if TriggerBank.event_inst.playback_state == PLAYBACK_STATE.STOPPED:
+            Trigger = False
