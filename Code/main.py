5e8a443a58679c129a85c898a39a868f99cb6ca8
BrezenFan <ozan.mgl@gmail.com>
2026-01-06 21:01:48 +0100

    ok Sound funktiert auch. aktueller Stand bei überschreiten von 15kmh wird ein Warnungssound gespielt

--- Code/main.py
+++ Code/main.py
@@ -16,7 +16,11 @@ if __name__ == '__main__':
     while True:
         Data.decode()
         Data.print_all()
-        if Data.get_speed() > Data.get_speed_limit() and Trigger == False:
+        # if Data.get_speed() > Data.get_speed_limit() and Trigger == False:
+        #     TriggerBank.event_inst.start()
+        #     Trigger = True
+        #Zum Test Speedlimit runter gesetzt:
+        if Data.get_speed() > 15 and Trigger == False:
             TriggerBank.event_inst.start()
             Trigger = True
         if TriggerBank.event_inst.playback_state == PLAYBACK_STATE.STOPPED:
