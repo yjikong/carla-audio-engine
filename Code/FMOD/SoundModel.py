7e28435e36262da9db9cc2f6031c15264adafaa1
BrezenFan <ozan.mgl@gmail.com>
2026-01-09 21:30:55 +0100

    Kommentar eingefügt, damit die eine Funktion nicht ausversehen gelöscht wird

--- Code/FMOD/SoundModel.py
+++ Code/FMOD/SoundModel.py
@@ -18,10 +18,12 @@ class SoundModel:
         while True:
             Data.decode()
             Data.print_all()
+            #richtige Funktion für späteren Gebrauch!
             # if Data.get_speed() > Data.get_speed_limit() and Trigger == False:
             #     TriggerBank.event_inst.start()
             #     Trigger = True
             #Zum Test Speedlimit runter gesetzt:
+            #Testfunktion um etwas zu hören
             if Data.get_speed() > 15 and Trigger == False:
                 TriggerBank.warning_sound.start()
                 Trigger = True
