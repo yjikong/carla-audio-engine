029827ae6892ff4e765025a9febb5635435b6490
BrezenFan <ozan.mgl@gmail.com>
2026-01-11 17:07:47 +0100

    Reverse_Beep Lautstärkeregler eingefügt

--- Code/FMOD/Classes/Sounds/Reverse_Beep.py
+++ Code/FMOD/Classes/Sounds/Reverse_Beep.py
@@ -10,35 +10,54 @@ class reverse_beep:
     grundton = None
     verzerrung = None
     channel = None
-    start_time = 0  # Merkt sich den Startzeitpunkt
+    start_time = 0
     is_playing = False
+    fader = None
 
     @staticmethod
     def init():
         if reverse_beep.system is None:
             reverse_beep.system = pyfmodex.System()
             reverse_beep.system.init()
+            lautstaerke_regler = reverse_beep.system.master_channel_group
+            lautstaerke_regler.volume = 0.1
 
     @staticmethod
     def dynamisch_Beep_erstellen():
         if reverse_beep.grundton is None:
+            # 1. Oszillator erstellen
             reverse_beep.grundton = reverse_beep.system.create_dsp_by_type(DSP_TYPE.OSCILLATOR)
-            reverse_beep.grundton.set_parameter_int(0, 0)
-            reverse_beep.grundton.set_parameter_float(1, 1500.0)
-            
-        if reverse_beep.verzerrung is None:
+            reverse_beep.grundton.set_parameter_int(0, 0)#Wellenform Sinus (0 bis 3 möglich)
+            reverse_beep.grundton.set_parameter_float(1, 1500.0)#Frequenz
+
+            # 2. Verzerrung erstellen
             reverse_beep.verzerrung = reverse_beep.system.create_dsp_by_type(DSP_TYPE.DISTORTION)
-            reverse_beep.verzerrung.set_parameter_float(0, 1.0)
+            reverse_beep.verzerrung.set_parameter_float(0, 1)#Verzerrungsfaktor
+
+            # 3. Fader erstellen
+            reverse_beep.fader = reverse_beep.system.create_dsp_by_type(DSP_TYPE.FADER)
+            reverse_beep.fader.set_parameter_float(0, 1)#Lautstärke
+
+            # 4. Kette erstellen
+            reverse_beep.verzerrung.add_input(reverse_beep.grundton)
+            # Verzerrung -> Fader
+            reverse_beep.fader.add_input(reverse_beep.verzerrung)
+
+            # Alle aktivieren
+            reverse_beep.grundton.active = True
+            reverse_beep.verzerrung.active = True
+            reverse_beep.fader.active = True
 
     @staticmethod
     def play_reverse_beep():
-        """Triggert den Beep nur, wenn er nicht schon läuft."""
         reverse_beep.init()
         reverse_beep.dynamisch_Beep_erstellen()
         
         if not reverse_beep.is_playing:
+            # Wir starten jetzt ganz normal
             reverse_beep.channel = reverse_beep.system.play_dsp(reverse_beep.grundton)
             reverse_beep.channel.add_dsp(0, reverse_beep.verzerrung)
+            
             reverse_beep.start_time = time.time()
             reverse_beep.is_playing = True
 
