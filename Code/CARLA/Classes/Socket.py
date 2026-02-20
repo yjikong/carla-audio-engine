08c78385625f4f1d965c85dcbcade979afaf92ff
BrezenFan <ozan.mgl@gmail.com>
2026-02-20 12:03:48 +0100

    Weather in in CarlaClient mit Threading eingebunden

--- Code/CARLA/Classes/Socket.py
+++ Code/CARLA/Classes/Socket.py
@@ -3,10 +3,9 @@ import json
 import time
 
 class Socket:
-    UDP_IP = "127.0.0.1"
-    UDP_PORT = 5005
-    socket = None
     def __init__(self):
+        self.UDP_IP = "127.0.0.1"
+        self.UDP_PORT = 5005
         self.socket = udp.socket(udp.AF_INET, udp.SOCK_DGRAM)
     def publish_data(self, data_packet):
         message = json.dumps(data_packet).encode()
--- Code/CARLA/cmain.py
+++ Code/CARLA/cmain.py
@@ -1,4 +1,5 @@
 import time
+from threading import Thread
 import sys
 from pathlib import Path
 
@@ -7,14 +8,16 @@ sys.path.append(str(PROJECT_ROOT))
 
 from Code.CARLA.Classes import *
 
-if __name__ == '__main__':
-    client = CarlaClient('localhost', 2000, 10.0)
-    weather = Weather(client)
-    sock = Socket()
-
-    weather.run()
-
+def carla_data_loop(client, sock):
     while True:
         data_packet = client.retrieve_data()
         sock.publish_data(data_packet)
         time.sleep(0.05)
+
+if __name__ == '__main__':
+    client = CarlaClient('localhost', 2000, 10.0)
+    weather = Weather(client)
+    sock = Socket()
+    carla_thread = Thread(target=carla_data_loop, args=(client, sock), daemon=True)
+    carla_thread.start()
+    weather.run()
